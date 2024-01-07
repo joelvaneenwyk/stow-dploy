76cfe3a11c6708f0d64179e67ea5ab187db48f41
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-07 15:21:44 -0800

    Remove pyfilesystem2 ('fs') dependency entirely
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- dploy/utils.py
+++ dploy/utils.py
@@ -3,20 +3,17 @@ A module that contains utility function mainly used for operations in the
 missing from the pathlib module.
 """
 
+from __future__ import print_function, unicode_literals
+
 import os
 import pathlib
 import shutil
 import stat
 from enum import Enum, auto
 from pathlib import Path
-from typing import Dict, Iterable, List, Optional, Union
+from typing import Dict, Iterable, List, Optional, Type, Union
 
-import fs
-import fs.osfs
-import fs.permissions
-from fs.errors import MissingInfoNamespace
-
-from dploy.oschmod import IS_WINDOWS, get_mode, set_mode
+from dploy.oschmod import get_mode, set_mode
 
 StowPath = Union[os.PathLike[str], str, Path]
 StowTreeIterable = Union[Dict[str, "StowTreeNode"], List["StowTreeNode"]]
@@ -24,24 +21,6 @@ StowTreeNode = Union[StowTreeIterable, StowPath]
 StowTree = StowTreeNode
 StowSources = Iterable[StowPath]
 StowIgnorePatterns = Optional[Iterable[str]]
-
-
-def _get_fs(path: StowPath) -> tuple[fs.osfs.OSFS, str]:
-    """
-    get a filesystem object from a path
-    """
-    try:
-        input_path_item = Path(path).resolve(strict=True)
-        root = fs.path.normpath(input_path_item.anchor)
-        rel = fs.path.normpath(str(input_path_item))
-        fs_mount = fs.osfs.OSFS(root, create=True, create_mode=0o777, expand_vars=True)
-        if IS_WINDOWS:
-            rel = rel.replace(root, "?", 1)
-            rel = rel.replace("\\", "/")
-        fs_path = fs.path.normpath(rel)
-    except IOError as io_error:
-        raise FileNotFoundError(f"Invalid file or directory: {path}") from io_error
-    return fs_mount, fs_path
 
 
 class Permission(int, Enum):
@@ -65,17 +44,311 @@ class Operation(Enum):
     REMOVE = auto()
 
 
-Permissions = Iterable[Permission]
 Permission_Read = [Permission.u_r, Permission.g_r, Permission.o_r]
 Permission_Write = [Permission.u_w, Permission.g_w, Permission.o_w]
 Permission_Execute = [Permission.u_x, Permission.g_x, Permission.o_x]
 
 
+def make_mode(init):
+    # type: (Union[int, Iterable[str], None]) -> int
+    """Make a mode integer from an initial value."""
+    return Permissions.get_mode(init)
+
+
+class _PermProperty(object):
+    """Creates simple properties to get/set permissions."""
+
+    def __init__(self, name):
+        # type: (str) -> None
+        self._name = name
+        self.__doc__ = "Boolean for '{}' permission.".format(name)
+
+    def __get__(self, obj, obj_type: Optional[Type["Permissions"]] = None):
+        return self._name in obj
+
+    def __set__(self, obj, value):
+        # type: (Permissions, bool) -> None
+        if value:
+            obj.add(self._name)
+        else:
+            obj.remove(self._name)
+
+
+class Permissions(object):
+    """An abstraction for file system permissions.
+
+    Permissions objects store information regarding the permissions
+    on a resource. It supports Linux permissions, but is generic enough
+    to manage permission information from almost any filesystem.
+
+    Example:
+        >>> from dploy.utils import Permissions
+        >>> p = Permissions(user='rwx', group='rw-', other='r--')
+        >>> print(p)
+        rwxrw-r--
+        >>> p.mode
+        500
+        >>> oct(p.mode)
+        '0o764'
+    """
+
+    _LINUX_PERMS: list[tuple[str, int]] = [
+        ("setuid", 2048),
+        ("setguid", 1024),
+        ("sticky", 512),
+        ("u_r", 256),
+        ("u_w", 128),
+        ("u_x", 64),
+        ("g_r", 32),
+        ("g_w", 16),
+        ("g_x", 8),
+        ("o_r", 4),
+        ("o_w", 2),
+        ("o_x", 1),
+    ]
+    _LINUX_PERMS_NAMES: list[str] = [_name for _name, _mask in _LINUX_PERMS]
+
+    def __init__(
+        self,
+        names=None,  # type: Optional[Iterable[str]]
+        mode=None,  # type: Optional[int]
+        user=None,  # type: Optional[str]
+        group=None,  # type: Optional[str]
+        other=None,  # type: Optional[str]
+        sticky=None,  # type: Optional[bool]
+        setuid=None,  # type: Optional[bool]
+        setguid=None,  # type: Optional[bool]
+    ):
+        # type: (...) -> None
+        """Create a new `Permissions` instance.
+
+        Arguments:
+            names (list, optional): A list of permissions.
+            mode (int, optional): A mode integer.
+            user (str, optional): A triplet of *user* permissions, e.g.
+                ``"rwx"`` or ``"r--"``
+            group (str, optional): A triplet of *group* permissions, e.g.
+                ``"rwx"`` or ``"r--"``
+            other (str, optional): A triplet of *other* permissions, e.g.
+                ``"rwx"`` or ``"r--"``
+            sticky (bool, optional): A boolean for the *sticky* bit.
+            setuid (bool, optional): A boolean for the *setuid* bit.
+            setguid (bool, optional): A boolean for the *setguid* bit.
+
+        """
+        if names is not None:
+            self._perms = set(names)
+        elif mode is not None:
+            self._perms = {name for name, mask in self._LINUX_PERMS if mode & mask}
+        else:
+            perms = self._perms = set()
+            perms.update("u_" + p for p in user or "" if p != "-")
+            perms.update("g_" + p for p in group or "" if p != "-")
+            perms.update("o_" + p for p in other or "" if p != "-")
+
+        if sticky:
+            self._perms.add("sticky")
+        if setuid:
+            self._perms.add("setuid")
+        if setguid:
+            self._perms.add("setguid")
+
+    def __repr__(self):
+        # type: () -> str
+        if not self._perms.issubset(self._LINUX_PERMS_NAMES):
+            _perms_str = ", ".join("'{}'".format(p) for p in sorted(self._perms))
+            return "Permissions(names=[{}])".format(_perms_str)
+
+        def _check(perm, name):
+            # type: (str, str) -> str
+            return name if perm in self._perms else ""
+
+        user = "".join((_check("u_r", "r"), _check("u_w", "w"), _check("u_x", "x")))
+        group = "".join((_check("g_r", "r"), _check("g_w", "w"), _check("g_x", "x")))
+        other = "".join((_check("o_r", "r"), _check("o_w", "w"), _check("o_x", "x")))
+        args = []
+        _fmt = "user='{}', group='{}', other='{}'"
+        basic = _fmt.format(user, group, other)
+        args.append(basic)
+        if self.sticky:
+            args.append("sticky=True")
+        if self.setuid:
+            args.append("setuid=True")
+        if self.setuid:
+            args.append("setguid=True")
+        return "Permissions({})".format(", ".join(args))
+
+    def __str__(self):
+        # type: () -> str
+        return self.as_str()
+
+    def __iter__(self) -> Iterable[str]:
+        return iter(self._perms)
+
+    def __contains__(self, permission):
+        # type: (object) -> bool
+        return permission in self._perms
+
+    def __eq__(self, other):
+        # type: (object) -> bool
+        if isinstance(other, Permissions):
+            names = other.dump()  # type: object
+        else:
+            names = other
+        return self.dump() == names
+
+    def __ne__(self, other):
+        # type: (object) -> bool
+        return not self.__eq__(other)
+
+    @classmethod
+    def parse(cls, ls):
+        # type: (str) -> Permissions
+        """Parse permissions in Linux notation."""
+        user = ls[:3]
+        group = ls[3:6]
+        other = ls[6:9]
+        return cls(user=user, group=group, other=other)
+
+    @classmethod
+    def load(cls, permissions):
+        # type: (List[str]) -> Permissions
+        """Load a serialized permissions object."""
+        return cls(names=permissions)
+
+    @classmethod
+    def create(cls, init=None):
+        # type: (Union[int, Iterable[str], None]) -> Permissions
+        """Create a permissions object from an initial value.
+
+        Arguments:
+            init (int or list, optional): May be None to use `0o777`
+                permissions, a mode integer, or a list of permission names.
+
+        Returns:
+            int: mode integer that may be used for instance by `os.makedir`.
+
+        Example:
+            >>> Permissions.create(None)
+            Permissions(user='rwx', group='rwx', other='rwx')
+            >>> Permissions.create(0o700)
+            Permissions(user='rwx', group='', other='')
+            >>> Permissions.create(['u_r', 'u_w', 'u_x'])
+            Permissions(user='rwx', group='', other='')
+
+        """
+        if init is None:
+            return cls(mode=0o777)
+        if isinstance(init, cls):
+            return init
+        if isinstance(init, int):
+            return cls(mode=init)
+        if isinstance(init, list):
+            return cls(names=init)
+        raise ValueError("permissions is invalid")
+
+    @classmethod
+    def get_mode(cls, init):
+        # type: (Union[int, Iterable[str], None]) -> int
+        """Convert an initial value to a mode integer."""
+        return cls.create(init).mode
+
+    def copy(self):
+        # type: () -> Permissions
+        """Make a copy of this permissions object."""
+        return Permissions(names=list(self._perms))
+
+    def dump(self):
+        # type: () -> List[str]
+        """Get a list suitable for serialization."""
+        return sorted(self._perms)
+
+    def as_str(self):
+        # type: () -> str
+        """Get a Linux-style string representation of permissions."""
+        perms = [c if name in self._perms else "-" for name, c in zip(self._LINUX_PERMS_NAMES[-9:], "rwxrwxrwx")]
+        if "setuid" in self._perms:
+            perms[2] = "s" if "u_x" in self._perms else "S"
+        if "setguid" in self._perms:
+            perms[5] = "s" if "g_x" in self._perms else "S"
+        if "sticky" in self._perms:
+            perms[8] = "t" if "o_x" in self._perms else "T"
+
+        perm_str = "".join(perms)
+        return perm_str
+
+    @property
+    def mode(self):
+        # type: () -> int
+        """`int`: mode integer."""
+        mode = 0
+        for name, mask in self._LINUX_PERMS:
+            if name in self._perms:
+                mode |= mask
+        return mode
+
+    @mode.setter
+    def mode(self, mode):
+        # type: (int) -> None
+        self._perms = {name for name, mask in self._LINUX_PERMS if mode & mask}
+
+    u_r = _PermProperty("u_r")
+    u_w = _PermProperty("u_w")
+    u_x = _PermProperty("u_x")
+
+    g_r = _PermProperty("g_r")
+    g_w = _PermProperty("g_w")
+    g_x = _PermProperty("g_x")
+
+    o_r = _PermProperty("o_r")
+    o_w = _PermProperty("o_w")
+    o_x = _PermProperty("o_x")
+
+    sticky = _PermProperty("sticky")
+    setuid = _PermProperty("setuid")
+    setguid = _PermProperty("setguid")
+
+    def add(self, *permissions):
+        # type: (*str) -> None
+        """Add permission(s).
+
+        Arguments:
+            *permissions (str): Permission name(s), such as ``'u_w'``
+                or ``'u_x'``.
+
+        """
+        self._perms.update(permissions)
+
+    def remove(self, *permissions):
+        # type: (*str) -> None
+        """Remove permission(s).
+
+        Arguments:
+            *permissions (str): Permission name(s), such as ``'u_w'``
+                or ``'u_x'``.s
+
+        """
+        self._perms.difference_update(permissions)
+
+    def check(self, *permissions):
+        # type: (*str) -> bool
+        """Check if one or more permissions are enabled.
+
+        Arguments:
+            *permissions (str): Permission name(s), such as ``'u_w'``
+                or ``'u_x'``.
+
+        Returns:
+            bool: `True` if all given permissions are set.
+
+        """
+        return self._perms.issuperset(permissions)
+
+
 def update_permissions(path: StowPath, operation: Operation, *permissions: Permission) -> None:
     """Add or remove permission(s) from a file or directory."""
     try:
-        os_file_system, input_path_item = _get_fs(path)
-        sys_path = os_file_system.getsyspath(input_path_item)
+        sys_path = Path(path).absolute()
         mode = get_mode(sys_path)
         for p in permissions:
             if operation == Operation.ADD:
@@ -83,8 +356,6 @@ def update_permissions(path: StowPath, operation: Operation, *permissions: Permi
             else:
                 mode &= ~p
         set_mode(sys_path, mode)
-    except MissingInfoNamespace:
-        pass
     except FileNotFoundError:
         pass
 
@@ -151,9 +422,8 @@ def get_relative_path(path: StowPath, start_at: StowPath) -> Path:
     return pathlib.Path(relative_path)
 
 
-def _get_access(path_item: StowPath) -> fs.permissions.Permissions:
-    mode = get_mode(path_item)
-    return fs.permissions.Permissions(mode=mode)
+def _get_access(path_item: StowPath) -> Permissions:
+    return Permissions(mode=get_mode(path_item))
 
 
 def is_file_readable(a_file: StowPath) -> bool:
